4530e7a96777a5be0682e4821fb58eb9eb81a316
Kondora Norbert <kondora35@gmail.com>
2024-10-27 13:44:19 +0200

    attribute init in contructor

--- main.cpp
+++ main.cpp
@@ -7,9 +7,11 @@ using namespace std;
 class Vehicle {
 private: 
     int nrOfWheels, hp;
-    char color[STRSIZE], brand[STRSIZE];
+    char color[STRSIZE] = "not set", brand[STRSIZE] = "not set";
 public:
-    Vehicle(int initial_nrOfWheels, int initial_hp, char *initial_color, char *initial_brand) {
+    Vehicle(){}
+
+    Vehicle(int initial_nrOfWheels, int initial_hp, const char *initial_color, const char *initial_brand) {
         nrOfWheels = initial_nrOfWheels;
         hp = initial_hp;
         strcpy(color, initial_color);
@@ -26,10 +28,13 @@ public:
 };
 
 int main(void) {
-    Vehicle vehicle1(4, 60, "red", "vw");
-
+    Vehicle vehicle1;
     cout << vehicle1.getVehicleBrand() << endl;
     cout << vehicle1.getVehicleColor() << endl;
+
+    Vehicle *vehicle2 = new Vehicle(4, 60, "red", "vw");
+    cout << vehicle2->getVehicleBrand() << endl;
+    cout << vehicle2->getVehicleColor() << endl;
     
     return 0;
 }
