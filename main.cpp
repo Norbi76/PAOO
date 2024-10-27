f22fb506905a77f7876fde19ac143cb1df19f444
Kondora Norbert <kondora35@gmail.com>
2024-10-27 13:53:23 +0200

    deconstructor

--- main.cpp
+++ main.cpp
@@ -6,16 +6,23 @@ using namespace std;
 
 class Vehicle {
 private: 
-    int nrOfWheels, hp;
-    char color[STRSIZE] = "not set", brand[STRSIZE] = "not set";
+    int *nrOfWheels, *hp;
+    char *color, *brand;
 public:
-    Vehicle(){}
-
     Vehicle(int initial_nrOfWheels, int initial_hp, const char *initial_color, const char *initial_brand) {
-        nrOfWheels = initial_nrOfWheels;
-        hp = initial_hp;
+        nrOfWheels = new int;
+        hp = new int;
+        color = new char[STRSIZE];
+        brand = new char[STRSIZE];
+        
+        *nrOfWheels = initial_nrOfWheels;
+        *hp = initial_hp;
         strcpy(color, initial_color);
         strcpy(brand, initial_brand);
+    }
+
+    ~Vehicle() {
+        delete[] nrOfWheels, hp, color, brand;
     }
 
     char *getVehicleColor() {
@@ -28,9 +35,9 @@ public:
 };
 
 int main(void) {
-    Vehicle vehicle1;
-    cout << vehicle1.getVehicleBrand() << endl;
-    cout << vehicle1.getVehicleColor() << endl;
+    // Vehicle vehicle1;
+    // cout << vehicle1.getVehicleBrand() << endl;
+    // cout << vehicle1.getVehicleColor() << endl;
 
     Vehicle *vehicle2 = new Vehicle(4, 60, "red", "vw");
     cout << vehicle2->getVehicleBrand() << endl;
