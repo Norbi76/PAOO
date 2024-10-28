0cc26eb2b8ab2e8f711c421f3917ad8de3d0a032
Kondora Norbert <kondora35@gmail.com>
2024-10-28 18:43:11 +0200

    created copy constructor

--- functions.hpp
+++ functions.hpp
@@ -1,11 +1,13 @@
 # define STRSIZE 10
 #include <cstring>
+using namespace std;
 
 class Vehicle {
 private: 
     int *nrOfWheels, *hp;
     char *color, *brand;
 public:
+    //Normal constructor
     Vehicle(int initial_nrOfWheels, int initial_hp, const char *initial_color, const char *initial_brand) {
         nrOfWheels = new int;
         hp = new int;
@@ -18,8 +20,30 @@ public:
         strcpy(brand, initial_brand);
     }
 
+    //Copy constructor
+    Vehicle(const Vehicle &obj) {
+        nrOfWheels = new int;
+        hp = new int;
+        color = new char[STRSIZE];
+        brand = new char[STRSIZE];
+
+        *nrOfWheels = *obj.nrOfWheels;
+        *hp = *obj.hp;
+        *color = *obj.color;
+        *brand = *obj.brand;
+    }
+
+    //Deconstructor 
     ~Vehicle() {
         delete[] nrOfWheels, hp, color, brand;
+    }
+
+    int getHp() {
+        return *hp;
+    }
+
+    int getNrOfWheels() {
+        return *nrOfWheels;
     }
 
     char *getVehicleColor() {
@@ -30,3 +54,10 @@ public:
         return brand;
     }
 };
+
+void printStr(char *str) {
+    for (int i = 0; i < strlen(str); i++) {
+        cout << str[i];
+    }
+    cout << endl;
+}
--- main.cpp
+++ main.cpp
@@ -3,13 +3,21 @@
 using namespace std;
 
 int main(void) {
-    // Vehicle vehicle1;
-    // cout << vehicle1.getVehicleBrand() << endl;
-    // cout << vehicle1.getVehicleColor() << endl;
+    Vehicle vehicle1(3, 50, "green", "bmw");
+    cout << "Vehicle 1 data:" << endl;
+    cout << vehicle1.getNrOfWheels() << endl;
+    cout << vehicle1.getHp() << endl;
+    cout << vehicle1.getVehicleBrand() << endl;
+    cout << vehicle1.getVehicleColor() << endl;
+    cout << endl;
 
-    Vehicle *vehicle2 = new Vehicle(4, 60, "red", "vw");
-    cout << vehicle2->getVehicleBrand() << endl;
-    cout << vehicle2->getVehicleColor() << endl;
+    Vehicle vehicle1_copy = vehicle1;
+    cout << "Vehicle 1 copy data:" << endl;
+    cout << vehicle1_copy.getNrOfWheels() << endl;
+    cout << vehicle1_copy.getHp() << endl;
+    cout << vehicle1.getVehicleBrand() << endl;
+    cout << vehicle1.getVehicleColor() << endl;
+        
     
     return 0;
 }
